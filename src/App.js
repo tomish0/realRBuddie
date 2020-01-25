5faf2c0d383cad1787af3722b798163d59fff7fb
Francesco <francescoitalia6@gmail.com>
2020-01-25 16:56:10 +0000

    Adds logic to hide the QR component after a succesful scan

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import QrReader from "react-qr-reader";
 import ShowReceipt from './component/ShowReceipt';
 import Button from './component/Button';
-import "./App.css";
+import "./styles/App.css";
+import logo from '../public/logo.png'
 
 class App extends Component {
   state = {
@@ -16,10 +17,15 @@ class App extends Component {
     console.log(this.state.receiptData);
     return (
       <div className="App">
-        <h1>Scan Your Receipt</h1>
+        <div className="App-top">
+          <img src={logo} alt="RBuddie Logo" className="App-logo" />
+
+        </div>
         {this.state.shouldScan ? <QrReader style={{ width: "100%" }} onScan={this.onScan} onError={this.onError} delay={300} facingMode="user" /> : <Button toggleQrReader={this.toggleReader} />}
         <div>{this.state.isError ? "This is not an RBuddie code" : ""}</div>
         {this.state.renderReceipt ? <ShowReceipt receipt={this.state.receiptData} isDuplicate={this.state.isDuplicate} /> : ''}
+        <header className="App-header">
+        </header>
       </div>
     );
   }
