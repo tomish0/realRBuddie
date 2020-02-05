5c89504399cf1097f5bbd6f919849680f2599e7b
Tom Isherwood <tomish0@outlook.com>
2020-02-05 19:21:17 +0000

    put button component into showReceipt component

---
+++ src/component/ShowAllReceipts.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ShowAllReceipts extends Component {
+    state = {  }
+    render() { 
+        return ( <div>hello</div> );
+    }
+}
+ 
+export default ShowAllReceipts;
--- src/component/ShowReceipt.jsx
+++ src/component/ShowReceipt.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./styles/ShowReceipt.css";
+import Button from "./Button";
 
 class ShowReceipt extends Component {
   state = {};
@@ -25,56 +26,59 @@ class ShowReceipt extends Component {
     });
 
     return (
-      <div className="full-receipt">
-        <div className="receipt-elements vendor">{receipt.vendor}</div>
-        <div className="store-info-section">
-          <div className="receipt-elements space-between">
-            <div>{receipt.storeName}</div>
-            <div>{receipt.storePhoneNo}</div>
-          </div>
-          <div className="receipt-elements space-between">
-            <div className="date-time">
-              <div>{receipt.purchaseDate}</div>
-              <div>{receipt.purchaseTime}</div>
+      <div>
+        <Button toggleQrReader={this.props.toggleQrReader} />
+        <div className="full-receipt">
+          <div className="receipt-elements vendor">{receipt.vendor}</div>
+          <div className="store-info-section">
+            <div className="receipt-elements space-between">
+              <div>{receipt.storeName}</div>
+              <div>{receipt.storePhoneNo}</div>
             </div>
-            <ul className="receipt-elements location">
-              <li>{receipt.storeLocation.company}</li>
-              <li>{receipt.storeLocation.road}</li>
-              <li>{receipt.storeLocation.town}</li>
-              <li>{receipt.storeLocation.postcode}</li>
-            </ul>
+            <div className="receipt-elements space-between">
+              <div className="date-time">
+                <div>{receipt.purchaseDate}</div>
+                <div>{receipt.purchaseTime}</div>
+              </div>
+              <ul className="receipt-elements location">
+                <li>{receipt.storeLocation.company}</li>
+                <li>{receipt.storeLocation.road}</li>
+                <li>{receipt.storeLocation.town}</li>
+                <li>{receipt.storeLocation.postcode}</li>
+              </ul>
+            </div>
+            <div className="receipt-elements space-between vat-number">
+              <div>VAT Number:</div>
+              <div>{receipt.vatNumber}</div>
+            </div>
           </div>
-          <div className="receipt-elements space-between vat-number">
-            <div>VAT Number:</div>
-            <div>{receipt.vatNumber}</div>
+          <div className="total-costs-section">
+            {eachItem}
+            <div className="receipt-elements space-between total-price">
+              <div>BALANCE DUE:</div>
+              <div>£{receipt.totalPrice}</div>
+            </div>
+            <div className="receipt-elements space-between tender-type">
+              <div>{receipt.tenderType}:</div>
+              <div>£{receipt.amountTendered}</div>
+            </div>
+            <div className="receipt-elements space-between change">
+              <div>CHANGE:</div>
+              <div>£{receipt.change}</div>
+            </div>
+            <div className="receipt-elements space-between auth-code">
+              <div>AUTH Code:</div>
+              <div>{receipt.authorisationCode}</div>
+            </div>
+            <div className="receipt-elements space-between vat">
+              <div>VAT (20%):</div>
+              <div>£{receipt.vatValue}</div>
+            </div>
           </div>
-        </div>
-        <div className="total-costs-section">
-          {eachItem}
-          <div className="receipt-elements space-between total-price">
-            <div>BALANCE DUE:</div>
-            <div>£{receipt.totalPrice}</div>
+          <div className="receipt-elements space-between receipt-app-id">
+            <div>{receipt.app}</div>
+            <div>id: {receipt.id}</div>
           </div>
-          <div className="receipt-elements space-between tender-type">
-            <div>{receipt.tenderType}</div>
-            <div>£{receipt.amountTendered}</div>
-          </div>
-          <div className="receipt-elements space-between change">
-            <div>CHANGE:</div>
-            <div>£{receipt.change}</div>
-          </div>
-          <div className="receipt-elements space-between auth-code">
-            <div>AUTH Code:</div>
-            <div>{receipt.authorisationCode}</div>
-          </div>
-          <div className="receipt-elements space-between vat">
-            <div>VAT (20%):</div>
-            <div>£{receipt.vatValue}</div>
-          </div>
-        </div>
-        <div className="receipt-elements space-between receipt-app-id">
-          <div>{receipt.app}</div>
-          <div>id: {receipt.id}</div>
         </div>
       </div>
     );
