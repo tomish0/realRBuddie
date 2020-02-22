a898b84dff5518f4628280e3a0fb3e93fae22745
Tom Isherwood <tomish0@outlook.com>
2020-02-22 16:56:52 +0000

    styling

--- src/component/FilterReceipts.jsx
+++ src/component/FilterReceipts.jsx
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
-import "../styles/Filter.css"
+import "../styles/Filter.css";
 
 class FilterReceipts extends Component {
   state = {};
   render() {
     return (
       <div className="filter">
-        <input
-          onChange={this.onInputChange}
-          type="text"
-          placeholder="Search by vendor"
-        />
-        {this.props.filteredDataError ? <p>You're filter failed so here are all your receipts</p> : null}
+        <div className="filter-input-i">
+          <i className="fas fa-search" />
+          <input
+            onChange={this.onInputChange}
+            type="text"
+            placeholder="Search by vendor"
+          />
+        </div>
+        <div className="filter-data-error">
+          {this.props.filteredDataError ? (
+            <p>You're filter failed so here are all your receipts</p>
+          ) : null}
+        </div>
       </div>
     );
   }
